666ed513d6661c18ff7b5382dded3f3baaddf70b
stzhao <stzhao@visa.com>
2021-03-24 11:52:02 -0500

    added atoi

--- src/test/java/LongestValidParenthesesTest.java
+++ src/test/java/LongestValidParenthesesTest.java
@@ -1,43 +1,14 @@
-//Given a string containing just the characters '(' and ')', find the length of 
-//the longest valid (well-formed) parentheses substring. 
-//
-// 
-// Example 1: 
-//
-// 
-//Input: s = "(()"
-//Output: 2
-//Explanation: The longest valid parentheses substring is "()".
-// 
-//
-// Example 2: 
-//
-// 
-//Input: s = ")()())"
-//Output: 4
-//Explanation: The longest valid parentheses substring is "()()".
-// 
-//
-// Example 3: 
-//
-// 
-//Input: s = ""
-//Output: 0
-// 
-//
-// 
-// Constraints: 
-//
-// 
-// 0 <= s.length <= 3 * 104 
-// s[i] is '(', or ')'. 
-// 
-// Related Topics String Dynamic Programming 
-// 👍 4559 👎 171
+import org.testng.annotations.Test;
 
+import java.util.Stack;
 
-//leetcode submit region begin(Prohibit modification and deletion)
-class Solution {
+public class LongestValidParenthesesTest {
+
+    @Test
+    public void testLongestValidParentheses(){
+        //")()())" expect 4
+    }
+
     public int longestValidParentheses(String s) {
         int max=0;
         Stack<Character> stack = new Stack<>();
@@ -78,6 +49,6 @@ class Solution {
             }
         }
         if(len>max) max=len;
-        return max;    }
+        return max;
+    }
 }
-//leetcode submit region end(Prohibit modification and deletion)
