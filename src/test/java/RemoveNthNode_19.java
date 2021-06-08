dc99cff05010993a3f2fea5f7b016f0a00d49fc3
songlunzhao <songlunzhao@gmail.com>
2021-06-08 12:07:46 -0500

    added RemoveNthNode

---
+++ src/test/java/RemoveNthNode_19.java
@@ -0,0 +1,44 @@
+import common.ListNode;
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Stack;
+
+import static org.testng.Assert.assertEquals;
+
+public class RemoveNthNode_19 {
+
+    @Test
+    public void testRemoveNthFromEnd() {
+
+
+
+    }
+
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        Stack<ListNode> stack = new Stack<>();
+        ListNode cur = head;
+        ListNode ret = head;
+        while (cur!=null) {
+            stack.push(cur);
+            cur=cur.next;
+        }
+        int i=0;
+        while (!stack.isEmpty()) {
+               ListNode node = stack.pop();
+               i++;
+               if(i==n){
+                   if(!stack.isEmpty()){
+                       ListNode node1 = stack.pop();
+                       node1.next=node.next;
+                   } else {
+                       ret = node.next;
+                   }
+               }
+        }
+
+        return ret;
+    }
+}
