27791cfd9955f9b7635c5baa9bb6d957b0923209
songlunzhao <songlunzhao@gmail.com>
2021-06-16 09:54:03 -0500

    added RotateList

--- docs/leetcode-solutions/leetcode/editor/en/[61]Rotate List.java
+++ docs/leetcode-solutions/leetcode/editor/en/[61]Rotate List.java
@@ -38,9 +38,35 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
+/**
+ * solution : link tail to head,
+ * then caculate where is the new tail of the list.
+ */
 class Solution {
     public ListNode rotateRight(ListNode head, int k) {
-        
+        if(head==null || head.next==null || k==0) return head;
+        ListNode cur = head, tail=null;
+        int len=0;
+        while(cur!=null){
+            len++;
+            if(cur.next==null){
+                tail=cur;
+            }
+            cur=cur.next;
+        }
+        int steps = k%len;
+        if(steps==0) return head;
+
+        tail.next=head; //make list a loop;
+`
+        cur=tail;
+        for(int i=0; i<len-steps; i++){
+            cur=cur.next;
+        }
+        head = cur.next;
+        cur.next=null;
+        return head;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
---
+++ src/test/java/RotateListTest_61.java
@@ -0,0 +1,42 @@
+import common.ListNode;
+import org.testng.annotations.Test;
+
+import java.util.*;
+
+
+public class RotateListTest_61 {
+
+    @Test
+    public void testRotateList(){
+
+    }
+
+    /**
+     * solution : link tail to head, make the linked list a loop,
+     * then calculate where is the new tail of the list.
+     */
+    public ListNode rotateRight(ListNode head, int k) {
+        if(head==null || head.next==null || k==0) return head;
+        ListNode cur = head, tail=null;
+        int len=0;
+        while(cur!=null){
+            len++;
+            if(cur.next==null){
+                tail=cur;
+            }
+            cur=cur.next;
+        }
+        int steps = k%len;
+        if(steps==0) return head;
+
+        tail.next=head;
+
+        cur=tail;
+        for(int i=0; i<len-steps; i++){
+            cur=cur.next;
+        }
+        head = cur.next;
+        cur.next=null;
+        return head;
+    }
+}
